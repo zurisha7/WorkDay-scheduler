90c26399e1864c42642447571f11247da1350f75
Zurisha <andreamwest7@gmail.com>
2022-04-22 21:12:26 -0500

    timeblocks created

---
+++ script.js
@@ -0,0 +1,27 @@
+
+var currentTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss a"); //get time
+        //display in p element
+     document.getElementById('currentDay').innerText = currentTime;
+
+        
+
+     
+    //  ar auditTask = function(taskEl) {
+    //     //get date for task element
+    //     var date = $(taskEl).find("span").text().trim();
+    
+        //convert to moment object at 5:00
+        var currentHour = moment().hour();
+    console.log(currentHour);
+       
+    
+//
+     
+
+
+
+
+
+
+ 
+
